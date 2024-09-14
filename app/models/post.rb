917c6cac18609c309dee4dc04205f437750178ee
Maciel <makjesus2008@gmail.com>
2024-09-14 12:33:39 -0300

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :title,:description ,presence:true
+    validates :title, length: {minimum:5, maximum:10}
+    validates :description, length: {minimum:5, maximum:10}
+
+    #before_save do
+      #self.title = self.title + "Maciel"
+    #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_145743) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_14_141645) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
